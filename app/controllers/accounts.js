258a45c591f14c420ad0e72e07aa3ecbbc3ef2ac
a-helen1 <andrewhelen1@gmail.com>
2020-06-09 00:08:46 +0100

    added password hashing and salting

--- app/controllers/accounts.js
+++ app/controllers/accounts.js
@@ -1,12 +1,15 @@
 'use strict';
 const User = require('../models/user');
 const Boom = require('@hapi/boom');
+const Joi = require('@hapi/joi');
+const bcrypt =require('bcrypt');
+const saltRounds = 10;
 
 const Accounts = {
     index: {
         auth: false,
         handler: function(request, h) {
-            return h.view('main', { title: 'Welcome to Points of Interes' });
+            return h.view('main', { title: 'Welcome to Points of Interest' });
         }
     },
     showSignup: {
@@ -17,6 +20,28 @@ const Accounts = {
     },
     signup: {
         auth: false,
+        validate: {
+            payload: {
+            firstName: Joi.string().required(),
+            lastName: Joi.string().required(),
+            email: Joi.string()
+              .email()
+              .required(),
+            password: Joi.string().required()
+        },
+        options: {
+            abortEarly: false
+        },
+        failAction: function(request, h, error) {
+            return h
+              .view('signup', {
+                  title: 'Sign up error',
+                  errors: error.details
+              })
+              .takeover()
+              .code(400);
+        }
+    },
         handler: async function(request, h) {
             try {
                 const payload = request.payload;
@@ -25,11 +50,14 @@ const Accounts = {
                     const message = 'Email address is already registered';
                     throw Boom.badData(message);
                 }
+
+                const hash = await bcrypt.hash(payload.password, saltRounds);
+
                 const newUser = new User({
                     firstName: payload.firstName,
                     lastName: payload.lastName,
                     email: payload.email,
-                    password: payload.password
+                    password: hash
                 });
                 user = await newUser.save();
                 request.cookieAuth.set({ id: user.id });
@@ -47,19 +75,44 @@ const Accounts = {
     },
     login: {
         auth: false,
+        validate: {
+            payload: {
+                email: Joi.string()
+                  .email()
+                  .required(),
+                password: Joi.string().required()
+            },
+            options: {
+                abortEarly: false
+            },
+            failAction: function(request, h, error) {
+                return h
+                  .view('login', {
+                      title: 'Sign in error',
+                      errors: error.details
+                  })
+                  .takeover()
+                  .code(400);
+            }
+        },
         handler: async function(request, h) {
-            const { email, password } = request.payload
-            let user = await User.findByEmail(email)
-            if(!user) {
-                const message = 'Email address is not registered';
-                throw Boom.unauthorized(message);
-                return h.redirect('/');
+            const { email, password } = request.payload;
+            try {
+                let user = await User.findByEmail(email);
+                if (!user) {
+                    const message = 'Email address is not registered';
+                    throw Boom.unauthorized(message);
+                }
+                if (!await user.comparePassword(password)) {
+                    const message = 'Password mismatch';
+                    throw Boom.unauthorized(message);
+                } else {
+                    request.cookieAuth.set({ id: user.id });
+                    return h.redirect('/home');
+                }
+            } catch (err) {
+                return h.view('login', { errors: [{ message: err.message }] });
             }
-            if (user.comparePassword(password)) {
-                request.cookieAuth.set({ id: user.id })
-                return h.redirect('/home');
-            }
-            return h.redirect('/');
         }
     },
     logout: {
